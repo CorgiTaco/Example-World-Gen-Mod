3fe133505a631e16a68ed9dae5b5af6438c46d9f
corgitaco <n/a>
2020-10-05 18:19:33 -0700

    Remove more stuff we dont need

--- src/main/java/corgitaco/exampleworldgenmod/ExampleWorldGenMod.java
+++ src/main/java/corgitaco/exampleworldgenmod/ExampleWorldGenMod.java
@@ -1,6 +1,5 @@
 package corgitaco.exampleworldgenmod;
 
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,11 +13,7 @@ public class ExampleWorldGenMod {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public ExampleWorldGenMod() {
-        // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event) {}
